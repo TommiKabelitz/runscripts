ed20205bc991a463f7dcdd68df28797f6706252b
Thomas Michael Kabelitz <a1724542@student.adelaide.edu.au>
2021-06-03 16:49:12 +0930

    useless CreateDirectory function removed

--- utilities.py
+++ utilities.py
@@ -1,59 +1,23 @@
+'''
+Some utility functions.
+
+'''
+
 import pathlib
 
 def WriteListLengthnList(fileObject,listToWrite):
+    '''
+    Writes the length of a list and then the contents to a file.
+
+    Arguments:
+    fileObject  -- fileObject: the file to write to
+    listToWrite -- list: The list to write to the file
+    '''
+
     length = len(listToWrite)
     fileObject.write(f'{length}\n')
     for element in listToWrite:
         fileObject.write(f'{element}\n')
-
-
-def CreateDirectory(paths,*args,**kwargs):
-    '''
-    Given a path including the file, makes the parent directory.
-
-    Arguments:
-    paths -- string: the path to the file or directory to create.
-                     Create multiple at once by passing a list or
-                     dict of paths.
-    '''
-    def create(fullFilePath):
-        '''
-        Sub-function to actually create the directory
-        '''
-        directory = IsolateDirectory(fullFilePath)
-        #Creating the directory if it does not exist
-        path = pathlib.Path(directory)
-        if path.exists() is False:
-            print(f'Making directory {directory}')
-            try:
-                path.mkdir(parents=True, exist_ok=True)
-            except PermissionError:
-                print(f'Permission to create {directory} denied')
-
-
-    #Dealing with input type
-    inputType = type(paths)
-    if inputType is str:
-        create(paths)
-    elif inputType is list:
-        for directory in paths:
-            create(directory)
-    elif inputType is dict:
-        for key,directory in paths.values():
-            create(directory)
-    else:
-        raise TypeError()
-
-
-def IsolateDirectory(fullFilePath,*args,**kwargs):
-    '''
-    Takes a file path and returns only the directory in which the file is located.
-    '''
-    #Find the last / in the path
-    index = fullFilePath.rfind('/')
-    #Slicing the string to isolate the directory
-    return fullFilePath[:index]
-
 
 
 def PrintDictToFile(filename,dictionary,order=None):
@@ -63,6 +27,7 @@ def PrintDictToFile(filename,dictionary,order=None):
         Warning: If order is not provided, the default order of the dictionary
                  is used. This is the insertion order from python 3.7, before
                  which that order cannot be guaranteed
+
         Arguments:
         filename -- string: The file to print to
         dictionary -- dict: The dict to print
