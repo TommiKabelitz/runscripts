d5437eb2361a6bd5085ed3425844ae8b0f5eca07
Thomas Michael Kabelitz <a1724542@student.adelaide.edu.au>
2021-06-11 09:32:00 +0930

    tidying

---
+++ phoenix_modules.sh
@@ -0,0 +1,8 @@
+#Modules for running COLA on the HPC1 cluster of the Adelaide University, Phoenix supercomputer
+
+module purge
+
+module load arch/haswell arch/skylake
+module load OpenMPI/1.8.8-iccifort-2015.3.187 CUDA/8.0.61
+
+module list
